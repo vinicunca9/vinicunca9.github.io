7e3b6f8fffab4278929fe81af3645fe05660581e
vinicunca9 <3156883370@qq.com>
2024-10-16 18:50:32 +0800

    Site updated: 2024-10-16 18:50:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/14/hello-world/"];function toRandomPost(){
+var posts=["2024/10/16/qingt/","2024/10/16/关于前端与后端通信的一些说明/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
