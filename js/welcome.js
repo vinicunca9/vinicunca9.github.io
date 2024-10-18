1eab9e187aec426ec7b0fce63d704eab19117e44
vinicunca9 <3156883370@qq.com>
2024-10-18 18:42:22 +0800

    Site updated: 2024-10-18 18:42:05

--- js/welcome.js
+++ js/welcome.js
@@ -36,7 +36,7 @@ function showWelcome() {
         return;
     }
 
-    let dist = getDistance(121.413921, 31.089290, ipLocation.data.lng, ipLocation.data.lat); // 修改自己的经度（121.413921）纬度（31.089290）
+    let dist = getDistance(115.72494899999992, 32.910622000000004, ipLocation.data.lng, ipLocation.data.lat); // 修改自己的经度（121.413921）纬度（31.089290）
     let pos = ipLocation.data.country;
     let ip = ipLocation.ip;
     let posdesc;
